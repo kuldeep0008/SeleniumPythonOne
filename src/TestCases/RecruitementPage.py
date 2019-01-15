ffddefa33b31e08c9357eea0489aac5e9783f581
Kuldeep Panwar <kuldeeppawar98@yahoo.in>
2019-01-15 18:13:14 +0530

    tempCommit

--- src/TestCases/RecruitementPage.py
+++ src/TestCases/RecruitementPage.py
@@ -2,8 +2,11 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.remote.webelement import WebElement
 from TestCases.ExcelRead import Excelread
 import pandas as pd
+import time
+import unittest
 
-class Recruitement_Page():
+
+class Recruitement_Page(unittest.TestCase):
     def __init__(self, driver):
         self.driver= driver
         self.Recruitment_Section_id='menu_recruitment_viewRecruitmentModule'
@@ -12,7 +15,7 @@ class Recruitement_Page():
     
     
     
-    def click_On_Recruitement_Page(self):
+    #def click_On_Recruitement_Page(self):
         self.driver.find_element_by_id(self.Recruitment_Section_id).click()
     
     
@@ -41,7 +44,7 @@ class Recruitement_Page():
         self.driver.find_element_by_id(self.job_Title_dropDown_id).click()
      
         select = Select(self.driver.find_element_by_xpath('//*[@id="candidateSearch_status"]'))
-        select.select_by_value("9")
+        select.select_by_value("SHORTLISTED")
  
  
         #Selecting Candidate name
@@ -50,18 +53,32 @@ class Recruitement_Page():
         exlrd = Excelread()
         data1 = exlrd.readExcelData()
         for a in data1:
-            for b in range(len(a)):
+            for b in range(0,1):
                 self.driver.find_element_by_xpath('//*[@id="candidateSearch_candidateName"]').send_keys(a[b])
                 
-                self.driver.find_element_by_xpath('//*[@id="candidateSearch_keywords"]').send_keys(a[b])
+                self.driver.find_element_by_xpath('//*[@id="candidateSearch_keywords"]').send_keys(a[b+1])
+                #self.driver.find_elements_by_xpath('//*[@id="candidateSearch_keywords"]').send_keys(Keys.ENTER)
                 
                 #self.driver.find_element_by_xpath('//*[@id="candidateSearch_fromDate"]').clear()
                 
-                self.driver.find_element_by_xpath('//*[@id="candidateSearch_fromDate"]').send_keys(a[b])
+                #self.driver.find_element_by_xpath('//*[@id="candidateSearch_fromDate"]').click()
+                
+                time.sleep(2)
+                
+                fromDate=self.driver.find_element_by_id('candidateSearch_fromDate')
+                fromDate.click()
+                fromDate.send_keys(a[b+2])
                 
                 #self.driver.find_element_by_xpath('//*[@id="candidateSearch_toDate"]').clear()
                 
-                self.driver.find_element_by_xpath('//*[@id="candidateSearch_toDate"]').send_keys(a[b])
+                
+                
+                time.sleep(2)
+                
+                toDate=self.driver.find_element_by_id('candidateSearch_toDate')
+                toDate.click()
+                toDate.send_keys(a[b+3])
+                
         
         
         
